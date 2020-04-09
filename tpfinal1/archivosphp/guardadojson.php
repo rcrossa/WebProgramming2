3815506f0b78b905c8af57aaf1a1cd6e7b937c1e
Roberto Rossa <rcrossa@hotmail.com>
2020-04-09 15:51:39 -0300

    update

--- tpfinal1/archivosphp/guardadojson.php
+++ tpfinal1/archivosphp/guardadojson.php
@@ -1,24 +1,24 @@
 <?php
-// //creo el json con el dato del formulario
-// $file= 'archivo.json';
-// //agregando elementos
-// array_push($_POST,105,"diez");
-// //codifico
-// $json_string=json_encode($_POST);
-// file_put_contents($file,$json_string. PHP_EOL, FILE_APPEND);
+//creo el json con el dato del formulario
+$file= 'archivo.json';
+//agregando elementos
+array_push($_POST,105,"diez");
+//codifico
+$json_string=json_encode($_POST);
+file_put_contents($file,$json_string. PHP_EOL, FILE_APPEND);
 
  
-// foreach ($_POST as $key => $value) {
-//     echo "<pre>";
-//     echo "El valor de " . $key . "es: " . $value;
-//     echo "</pre>";
-// }
+foreach ($_POST as $key => $value) {
+    echo "<pre>";
+    echo "El valor de " . $key . "es: " . $value;
+    echo "</pre>";
+}
 
 
-//---------------------------------
+---------------------------------
  
-// 
 
-// <form action="<?php echo $_SERVER['PHP_SELF'] " method="post"> 
-// echo "-----------";
+
+<form action="<?php echo $_SERVER['PHP_SELF'] " method="post"> 
+echo "-----------";
 ?>
