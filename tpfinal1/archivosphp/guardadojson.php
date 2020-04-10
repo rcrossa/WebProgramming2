72367ac7e1cfc45a48ea584ba4c4b4de96d22a49
Roberto Rossa <rcrossa@hotmail.com>
2020-04-10 13:00:53 -0300

    update
    Se actualiza archivo guardado json

--- tpfinal1/archivosphp/guardadojson.php
+++ tpfinal1/archivosphp/guardadojson.php
@@ -4,13 +4,16 @@
 $file= 'archivo.json';
 
 //array de ejemplo recibido
-$post =["java","44"];
+$post =['java',"1"];
 //defino variable
 $a1=0;
 $java;
 $java1=0;
 $voto;
+$voto1=array();
 $c=array();
+$votoextraido;
+$a=array();
 echo "----------------";
 //recorro
 foreach ($post as $key => $value) {
@@ -25,52 +28,79 @@ echo "extraigo los datos del array post";
 echo "<br>";
 
 extract($post);
-echo "\$java = $post[0] \$java1=$post[1]";
-// $java=$post[0];
-// $java2=$post[1];
+echo "\$java=$post[0] \$voto=$post[1]";
+echo "<br>";
+echo "---------------";
+echo "<br>";
+echo "asigno las posiciones del array a variables";
+$java=$post[0];
+echo "<br>";
+$voto=$post[1];
+echo "imprimo juego: ".$java." ,imprimo voto: ".$voto;
+echo "<br>";
+echo "prueba ok";
+echo "<br>";
 echo "<br>";
 echo "----------------";
 echo "<br>";
-
 //obtengo el contenido
 $json_string=file_get_contents($file);
 //decodifico
-$a=json_decode($json_string);
-// extraigo la y separo los datos del archivo
+$voto1=json_decode($json_string);
 
-// extract($a);
-//  echo "\$a1 =$a[0]";
-//  $a1 =$a[0]+$java1;
+echo "imprimo lo que esta dentro los valores del archivo";
+echo "<br>";
+foreach ($voto1 as $key =>$votoextraido) {
+    print_r($votoextraido);
+}
+ echo "<br>"."<br>";
+ echo "compruebo que la variable tenga valor";
+echo "<br>";
+ echo "".$votoextraido;
+ echo "<br>";
+echo "<br>";
+//asigno los datos a un array
 
-// echo "<br>";
+echo  "sumo el voto extraido y el del array post";
+$voto=$voto+$votoextraido;
+echo "<br>";
+echo "valor de voto del extraido del json es: ".$votoextraido;
+echo "<br>";
+echo " la suma de los votos es : ".$voto;
+echo "<br>";
 
-// //imprimo los dos datos juntos
-// echo " java: ".$java." tiene ". " voto: ".$a1;
-// echo "<br>";
-// echo "----------------";
-// echo "<br>";
-$c=array_push($java,'$java1');
+
+echo "<br>";
+echo "----------------";
+echo "<br>";
+echo "junto valores en un nuevo array";
+echo "<br>";
+// $a=['$java'=> "java",'$voto'=>"$voto"];
+// $a[]=$java,$voto;
+// array_push($a,$java,$voto);
+array_unshift($a,'$java',"$voto");
+// array_push($a,$java,$voto);
 //sumo y junto los datos en un nuevo array
 // $d=$java+$a1;
 // echo"".$d;
 // $java1=array("java","voto");
 // $c=array_combine("$java","$java2","$a1");
-// print_r($c);
+// print_r($a);
 // $c=array_combine($java1,$d);
 
 // $json_string= ["java","voto"];
 // $a=array_combine($java1,$c);
-print_r($c);
+print_r($a);
 
-// //codifico el array
-$json_string=json_encode($c);
-// //guardo en el archivo json
-file_put_contents($file,$c. PHP_EOL,FILE_APPEND);
+// // //codifico el array
+// $json_string=json_encode($a);
+// // //guardo en el archivo json
+// file_put_contents($file,$json_string. PHP_EOL);
 
 //codifico el array
-// $json_string1=json_encode($c);
+$json_string1=json_encode($a);
 //guardo en el archivo json
-// file_put_contents($file,$json_string1. PHP_EOL);
+file_put_contents($file,$json_string1. PHP_EOL);
 echo "<br>";
 echo "----------------";
 echo "<br>";
