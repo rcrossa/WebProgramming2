f034fcc9d5c15072a491ad8196b849a5cd2c64b6
francodsv31 <63472853+francodsv31@users.noreply.github.com>
2020-04-11 18:56:23 -0300

    Add files via upload
    archivos anexos para detalle.php

---
+++ tpfinal1/archivosphp/slidedetalle.php
@@ -0,0 +1,26 @@
+  <!--Control de Slides-->
+  <section>
+    <div class="container">
+      <div id="CarruselControl" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#CarruselControl" data-slide-to="0" class="active"></li>
+          <li data-target="#CarruselControl" data-slide-to="1"></li>
+          <li data-target="#CarruselControl" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <a href="#!"><img class="d-block w-100 img-fluid" id="slide" src="images/MASVENDIDOS.jpg"
+                alt="Juegos Mas Vendidos" width="1920" height="800"></a>
+          </div>
+          <div class="carousel-item">
+            <a href="#!"><img class="d-block w-100 img-fluid" id="slide_dos" src="images/NUEVOSLANZAMIENTOS.jpg"
+                alt="Nuevos lanzamientos" width="1920" height="800"></a>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100 img-fluid" id="slide_tres" src="images/SLIDE3.jpg" alt="Tercer Slide" width="1920"
+              height="270">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
